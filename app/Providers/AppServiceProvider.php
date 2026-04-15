4334718ca8349cfec8a7d92ad7adc7a28bf0f2a9
delacroix <delacroix.jon@gmail.com>
2026-04-15 10:23:37 +0200

    Activité 6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\Product;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('manage-product', function (User $user, Product $product) {
+            return $product->user_id === $user->id;
+        });
+
+        Gate::define('view-product', function (?User $user, Product $product) {
+            return $product->is_public || ($user && $product->user_id === $user->id);
+        });
     }
 }
